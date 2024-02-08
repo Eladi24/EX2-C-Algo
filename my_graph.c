7389102f34a74723285426632852316de369163b
Elad Imany <vboxuser@Ubuntu-22.03.4.myguest.virtualbox.org>
2024-02-08 17:16:39 +0200

    update

--- my_graph.c
+++ my_graph.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 #include "my_mat.h"
 
-
-
-int main() {
+int main()
+{
     char choise;
-    do {
-        //printf("Please enter your choise: ");
+    do
+    {
+        // printf("Please enter your choise: ");
         scanf("%c", &choise);
 
         switch (choise)
         {
         case 'A':
+        {
             createAdjMatrix();
             break;
-            
+        }
         case 'B':
+        {
             isPath();
             break;
-
+        }
         case 'C':
+        {
             shortestPath();
             break;
-
-        case 'D':
-            break;
-
-        default:
-            
         }
-    } 
-    while (choise != 'D' && choise != EOF); 
-    
+        }
+    } while (choise != 'D' && choise != EOF);
+
     return 0;
-    
 }
-
-
-
